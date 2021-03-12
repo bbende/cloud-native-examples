b9a286f901122c200792fca8a3cb09dc02eded35
Bryan Bende <bbende@gmail.com>
2021-03-11 22:12:58 -0500

    Initial project structure for example app

---
+++ example-app/example-app-backend/src/test/java/com/bbende/k8s/example/app/backend/ExampleAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbende.k8s.example.app.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ example-app/example-app-frontend/src/main/java/com/bbende/k8s/example/frontend/ExampleAppFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.bbende.k8s.example.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleAppFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleAppFrontendApplication.class, args);
+	}
+
+}
