e1f29ad458efb9f8dfd4e3b4ae17b68df95b8a2e
Bryan Bende <bbende@gmail.com>
2021-07-05 21:20:31 -0400

    Display build timestamp in frontend UI

--- example-app/example-frontend/src/main/java/com/bbende/k8s/example/frontend/message/MessageMvcController.java
+++ example-app/example-frontend/src/main/java/com/bbende/k8s/example/frontend/message/MessageMvcController.java
@@ -1,6 +1,9 @@
 package com.bbende.k8s.example.frontend.message;
 
 import com.bbende.k8s.example.api.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,15 +11,27 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Controller
 public class MessageMvcController {
 
-    private MessageClientService messageClientService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageMvcController.class);
 
-    public MessageMvcController(final MessageClientService messageClientService) {
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter
+            .ofPattern("yyyy-MM-dd HH:mm:ss")
+            .withZone(ZoneId.systemDefault());
+
+    private MessageClientService messageClientService;
+    private BuildProperties buildProperties;
+
+    public MessageMvcController(final MessageClientService messageClientService,
+                                final BuildProperties buildProperties) {
         this.messageClientService = messageClientService;
+        this.buildProperties = buildProperties;
     }
 
     @GetMapping("/")
@@ -24,8 +39,12 @@ public class MessageMvcController {
         try {
             final List<Message> messages = messageClientService.getAll();
             final ModelMap modelMap = new ModelMap("messages", messages);
+
+            final Instant buildInstant = buildProperties.getTime();
+            modelMap.addAttribute("buildTimestamp", DATE_TIME_FORMATTER.format(buildInstant));
             return new ModelAndView("index", modelMap);
         } catch (Throwable t) {
+            LOGGER.error(t.getMessage(), t);
             final ModelMap modelMap = new ModelMap("error", t.getMessage());
             return new ModelAndView("error", modelMap);
         }
@@ -44,6 +63,7 @@ public class MessageMvcController {
             messageClientService.add(message);
             return new ModelAndView("redirect:/");
         } catch (Throwable t) {
+            LOGGER.error(t.getMessage(), t);
             final ModelMap modelMap = new ModelMap("error", t.getMessage());
             return new ModelAndView("error", modelMap);
         }
